42ce1c26a6a125c0a39dda0f19c9e0047646c7c4
Filzah Mufidah <filzahmufidah@192.168.1.2>
2025-02-06 10:58:27 +0700

    app typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
   <div>
